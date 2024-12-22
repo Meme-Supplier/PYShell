016b5bcd26fedfb6cf934e21653bf900fb06c757
Meme Guy <owen.debiasio@gmail.com>
2024-12-22 15:20:33 -0500

    Update help.py
    Removed unnecessary entries.

--- pscripts/help.py
+++ pscripts/help.py
@@ -1,9 +1,5 @@
 print("Available commands:\n")
 
-print("!!!IF YOU WANT TO ENABLE WIP COMMANDS!!!")
-print("beta true")
-print("beta false (if you want to turn it off)")
-
 print("echo <text>")
 print("exit")
 print('clear')
@@ -11,12 +7,6 @@ print('help')
 print("python")
 print("neofetch")
 print("shutdown")
-print("run <.run file>")
 print("app <app>")
-
-# Beta commands
-print("cd <directory>")
-print("ls")
-print("dir")
-
-print()
+print("expr <expression>")
+print('ver\n')
