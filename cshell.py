8994237dcd9b100f589aaed4b9e829fe1666d9e3
Meme Guy <owen.debiasio@gmail.com>
2024-12-22 15:22:02 -0500

    Changes to cshell.py
    New commands and cleaned up code

--- cshell.py
+++ cshell.py
@@ -1,58 +1,43 @@
-import os, sys, subprocess
-
+import os, sys, subprocess, platform
 os.system('clear')
-unstable = False
 
-while True:
-    answer = input("cshell$~: ")
+def command(command):
+    subprocess.run([command], shell = True)
 
-    if answer.startswith("echo "):  # Check if the input starts with "echo "
-        echo = answer.replace("echo ", "", 1)  # Remove the first "echo " only
+if platform.system() != 'Linux':
+    print("This script is for Linux only! Either install Linux for edit the code (it may not work if you edit the code)!")
+
+while True and platform.system() == 'Linux':
+    answer = input('cshell$~: ')
+
+    if answer.startswith("echo "):
+        echo = answer.replace("echo ", "", 1)
         print(echo)
-    elif answer.startswith("beta "):
-        beta = answer.replace("beta ", "", 1)
-        if beta == "true":
-            unstable = True
-            print("Unstable commands enabled")
-        else:
-            unstable = False
-            print("Unstable commands disabled")
-    if unstable == True:    
-        if answer.startswith("cd "):
-            cd = answer.replace("cd ", "", 1)
-            print(cd)
-            subprocess.run(['cd ',cd], shell = True)
-        elif answer == 'dir':
-            subprocess.run(['dir'], shell = True)
-        elif answer == 'list':
-            subprocess.run(['ls'], shell = True)
-        elif answer.startswith("run "): 
-            run = answer.replace("run ", "", 1)
-            subprocess.run(['chmod +x ',
-                            run,
-                            " ./runtest.run"], shell = True)
+    elif answer.startswith("expr ") or answer == 'neofetch':
+        command(answer)
     elif answer == "clear":
         os.system(answer)
-    elif answer == 'help':
+    elif answer.startswith("help"):
         import pscripts.help
     elif answer == 'exit':
         sys.exit()
     elif answer == 'python':
         try:
-            subprocess.run(['python3'], shell = True)
+            command('python3')
         except:
             print("Python not found. Would you like to install it?\n")
             x = input("")
             if x == 'Y' or x == 'y':
-                subprocess.run(['sudo apt install python3'], shell = True)
+                command('sudo apt install python3')
             else:
                 print("Operation canceled.")
-    # Neofetch
-    elif answer == "neofetch":
-        subprocess.run(['neofetch'], shell = True)
-    # Shutdown
     elif answer == "shutdown":
-        subprocess.run(['shutdown now'], shell = True)
+        command('shutdown now')
     elif answer.startswith("app "):
         app = answer.replace("app ", "", 1)
-        subprocess.run([app], shell = True)
+        command(app)
+    elif answer == 'ver':
+        print('\nCSHELL version: v0.2')
+        print(platform.system(),platform.release(),'\n')
+    else:
+        print('Invalid command')
