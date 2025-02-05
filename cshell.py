c63679329fdd4919ed832aa8d529cbf5d5ab9f17
Meme Guy <owen.debiasio@gmail.com>
2025-02-05 17:12:26 -0500

    HUGE Update cshell.py
    View "Releases" to see the changes

--- cshell.py
+++ cshell.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 """
 # 2025 Meme Supplier
@@ -12,16 +13,16 @@ import platform
 import webbrowser
 import shutil
 
-os.system('clear')
+os.system("clear")
 
 currentDirectory = os.path.dirname(os.path.abspath(__file__))
 
-pythonMajor = sys.version_info.major  # Ex: 3.
-pythonMinor = sys.version_info.minor  # Ex: .12
-pythonMicro = sys.version_info.micro
+pythonMajor = sys.version_info.major  # Ex: 3.x.x
+pythonMinor = sys.version_info.minor  # Ex: x.12.x
+pythonMicro = sys.version_info.micro # Ex: x.x.3
 pythonVersion = str(pythonMajor) + "." + str(pythonMinor) + "." + str(pythonMicro)
 
-cshellVer = "v0.6"
+cshellVer = "v0.7"
 
 locked = False
 passwordSet = False
@@ -38,68 +39,87 @@ def commands():
     """
     
     print('\n' +
-#             Color        Output          
-            Fore.YELLOW + "Available commands:\n")
+#         Color        Output          
+          Fore.YELLOW + "Available commands:\n")
 #   Exit
     print(Fore.CYAN + "exit " +
-            Fore.GREEN + "Exits CShell")
+          Fore.GREEN + "Exits CShell")
 #   Clear
     print(Fore.CYAN + "clear" +
-            Fore.GREEN + " Clears the screen")
+          Fore.GREEN + " Clears the screen")
 #   Help
     print(Fore.CYAN + "help" +
-            Fore.GREEN + " Some info")
+          Fore.GREEN + " Some info")
 #   Cmds
     print(Fore.CYAN + "cmds" +
-            Fore.GREEN + " Lists available commands")
+          Fore.GREEN + " Lists available commands")
 #   Python
     print(Fore.CYAN + "python" +
-            Fore.GREEN + " Opens python")
+          Fore.GREEN + " Opens python")
 #   Ver
     print(Fore.CYAN + "ver" +
-            Fore.GREEN + " Shows CShell's version")
+          Fore.GREEN + " Shows CShell's version")
 #   Lock
     print(Fore.CYAN + "lock" +
-            Fore.GREEN + " Locks the terminal")
+          Fore.GREEN + " Locks the terminal")
 #   Edit
     print(Fore.CYAN + "edit" +
-            Fore.GREEN + " Allows you to easily edit CShell")
+          Fore.GREEN + " Allows you to easily edit CShell")
 #   Reload
     print(Fore.CYAN + "reload" +
-            Fore.GREEN + " Reloads CShell")
+          Fore.GREEN + " Reloads CShell")
+#   Reload
+    print(Fore.CYAN + "uptime" +
+          Fore.GREEN + " Shows how long your system has been on")
 #   Shutdown
     print(Fore.CYAN + "shutdown" +
-            Fore.GREEN+ " Shuts down your system")
+          Fore.GREEN+ " Shuts down your system")
+#   Uninstall
+    print(Fore.CYAN + "uninstall" +
+          Fore.GREEN+ " Uninstalls CShell.")
 #   Wait
     print(Fore.CYAN + "wait"     +
-            Fore.BLUE + " <time (seconds)>" +
-            Fore.GREEN + " Waits your desired time")
+          Fore.BLUE + " <time (seconds)>" +
+          Fore.GREEN + " Waits your desired time")
 #   Echo
     print(Fore.CYAN + "echo"     +
-            Fore.BLUE + " <text>" +
-            Fore.GREEN + " Prints text")
-#   Command
-    print(Fore.CYAN + "command " +
-            Fore.BLUE +"<app>" +
-            Fore.GREEN + " Runs normal shell commands " +
-            Fore.RED + "Notice: commands like \"cd\" and \"dir\" won't work.")
+          Fore.BLUE + " <text>" +
+          Fore.GREEN + " Prints text")
+#   Bash
+    print(Fore.CYAN + "bash " +
+          Fore.BLUE + "<commands>" +
+          Fore.GREEN + " Runs normal shell commands " +
+          Fore.RED + "Notice: commands like \"cd\" and \"dir\" won't work.")
 #   Expr
     print(Fore.CYAN + "expr "    +
-            Fore.BLUE + "<equation>" +
-            Fore.GREEN + " Solves a math equation")
+          Fore.BLUE + "<equation>" +
+          Fore.GREEN + " Solves a math equation")
 #   Web
     print(Fore.CYAN + "web "     +
-            Fore.BLUE + "<website>" +
-            Fore.GREEN + " Opens your desired website")
+          Fore.BLUE + "<website>" +
+          Fore.GREEN + " Opens your desired website")
+#   Script
+    print(Fore.CYAN + "script "     +
+          Fore.BLUE + "<path to " +
+          Fore.BLUE + "file>" +
+          Fore.GREEN + " Runs a CShell script")
+#   In
+    print(Fore.CYAN + "in "     +
+          Fore.BLUE + "<text>" +
+          Fore.GREEN + " Recieves confimation from user")
+#   Ls
+    print(Fore.CYAN + "ls "     +
+          Fore.BLUE + "<directory>" +
+          Fore.GREEN + " Lists the specified directory")
 #   Pwd
     print(Fore.CYAN + "pwd "     +
-            Fore.BLUE + "<password>" +
-            Fore.GREEN + " Sets a password " +
-            Fore.RED + "Notice: Password has to be at least 5 characters long.")
+          Fore.BLUE + "<password>" +
+          Fore.GREEN + " Sets a password " +
+          Fore.RED + "Notice: Password has to be at least 5 characters long.")
 #   Update
     print(Fore.CYAN + "update "     +
-            Fore.GREEN + " Updates your device " +
-            Fore.RED + "Notice: Only supports Fedora, Arch, and Debian based distros.\n")
+          Fore.GREEN + " Updates your device " +
+          Fore.RED + "Notice: Only supports Fedora, Arch, and Debian based distros.\n")
 
 def processCommand(answer):
 #       Goes through and executes commands
@@ -117,38 +137,49 @@ def processCommand(answer):
         case "lock"     : lock()
         case "edit"     : edit()
         case "reload"   : reload()
+        case "in"       : input()
+        case "uptime"   : command("uptime")
+        case "uninstall": uninstall()
 
-        #Commands that require syntax
-        #(Show usage)
+#       Commands that require syntax
+#       (Show usage)
 
         case "echo"    : print(Fore.CYAN + "Usage: " +
-                                Fore.BLUE + "echo <message>")
+                               Fore.BLUE + "echo <message>")
         case "expr"    : print(Fore.CYAN + "Usage: " +
-                                Fore.BLUE + "expr <equation>")
-        case "command" : print(Fore.CYAN + "Usage: " +
-                                Fore.BLUE + "command <command>")
+                               Fore.BLUE + "expr <equation>")
+        case "bash"    : print(Fore.CYAN + "Usage: " +
+                               Fore.BLUE + "bash <command>")
         case "web"     : print(Fore.CYAN + "Usage: " +
-                                Fore.BLUE + "web <website>")
+                               Fore.BLUE + "web <website>")
         case "wait"    : print(Fore.CYAN + "Usage: " +
-                                Fore.BLUE + "wait <time (seconds)>")
+                               Fore.BLUE + "wait <time (seconds)>")
         case "pwd"     : print(Fore.CYAN + "Usage: " +
-                                Fore.BLUE + "pwd <password>")
+                               Fore.BLUE + "pwd <password>")
+        case "script"  : print(Fore.CYAN + "Usage: " +
+                               Fore.BLUE + "script <script path>")
+        case "ls"      : print(Fore.CYAN + "Usage: " +
+                               Fore.BLUE + "ls <directory>")
             
         case ''        : () # Does nothing
 
         case _ :
-
-#               Multi-syntax
+#           Multi-syntax
             if   answer.startswith ("echo ")    : command(answer)
             elif answer.startswith ("expr ")    : command(answer)
-            elif answer.startswith ("command ") : command(answer.replace("command " , "" , 1))
+            elif answer.startswith ("bash    ") : command(answer.replace("bash " , "" , 1))
             elif answer.startswith ("web ")     : webbrowser.open_new(answer.replace("web " , "" , 1))
             elif answer.startswith ("wait ")    : wait(answer.replace("wait " , "" , 1))
             elif answer.startswith ("pwd ")     : setPwd(answer.replace("pwd " , "" , 1))
-#               If nothing checks out
+            elif answer.startswith ("script ")  : script(answer.replace("script " , "" , 1))
+            elif answer.startswith ("in ")      : input(answer.replace("in " , "" , 1) + '\n')
+            elif answer.startswith ("ls ")      : ls(answer.replace("ls " , "" , 1) + '\n')
+            elif answer.startswith ("flatpak")  : command(answer)
+
+#           If nothing checks out
             else: print(Fore.RED +
                     answer +
-                    ": command or program not found")
+                    ": invalid command.")
 
 """
 Info
@@ -156,25 +187,25 @@ Info
 
 def credits():
     print(Fore.CYAN  + "Meme Supplier" +
-            Fore.BLUE  + ": owner, programmer, maintainer\n" +
-            Fore.GREEN + "2025 Meme Supplier")
+          Fore.BLUE  + ": owner, programmer, maintainer\n" +
+          Fore.GREEN + "2025 Meme Supplier")
 
 def help():
 #             Color         Output
     print(Fore.CYAN   + "Welcome to " +
-            Fore.GREEN  + "CShell " +
-            Fore.YELLOW + cshellVer)
+          Fore.GREEN  + "CShell " +
+          Fore.YELLOW + cshellVer)
     print(Fore.CYAN   + "Type " +
-            Fore.BLUE   + "\"cmds\"" +
-            Fore.CYAN   + " for some commands!\n")
+          Fore.BLUE   + "\"cmds\"" +
+          Fore.CYAN   + " for some commands!\n")
 
 def ver():
 #             Color        Output
     print(Fore.CYAN  + '\nCSHELL' +
-            Fore.BLUE  + ' version: ' +
-            Fore.GREEN + cshellVer)
+          Fore.BLUE  + ' version: ' +
+          Fore.GREEN + cshellVer)
     print(Fore.CYAN  + platform.system() ,
-            Fore.BLUE  + platform.release())
+          Fore.BLUE  + platform.release())
     print(Fore.CYAN + "Python " +
           Fore.BLUE + sys.version + '\n')
 
@@ -182,6 +213,19 @@ def ver():
 Scripting
 Commands
 """
+
+def ls(lsDir):
+    command("ls " + lsDir)
+
+def uninstall():
+    choice = input(Fore.RED + "Do you want to uninstall CShell?\n" +
+                   Fore.WHITE)
+    
+    if choice == 'Y' or choice == 'y':
+        command("bash ~/cshell/uninstall.sh")
+        sys.exit(0)
+    else:
+        print("\nAborted.")
 
 def wait(value):
     command("sleep " + value)
@@ -193,9 +237,8 @@ def setPwd(pwd):
     global password , passwordSet
 
     if len(pwd) < 5: print(Fore.RED  + "Password must be at least " +
-                            Fore.BLUE + "5 " +
-                            Fore.RED  + "characters long!")
-                            
+                           Fore.BLUE + "5 " +
+                           Fore.RED  + "characters long!")
     else:
         password = pwd
         passwordSet = True
@@ -216,20 +259,23 @@ def lock():
     else:
         print(Fore.RED + "You need to set a password first in order to use this command.")
         print(Fore.RED + "Use the command " +
-                Fore.BLUE + "\"pwd <password>\" " + 
-                Fore.RED + "to set your password")
+              Fore.BLUE + "\"pwd <password>\" " + 
+              Fore.RED + "to set your password")
     
 
 def update():
-    if   shutil.which("apt"):    command("sudo apt update && sudo apt upgrade")
-    elif shutil.which("dnf"):    command("sudo dnf update")
-    elif shutil.which("pacman"): command("sudo pacman -Syu")
+    if shutil.which("apt"):   
+        command("sudo apt update && sudo apt upgrade")
+    elif shutil.which("dnf"):   
+        command("sudo dnf update")
+    elif shutil.which("pacman"):
+        command("sudo pacman -Syu")
     else:
         print(Fore.RED + "Unsupported package manager!")
 
 def edit():
     try:    
-        command("cd " + currentDirectory + " && sudo nano cshell.py")
+        command("sudo nano /home/meme/cshell/cshell.py")
     except:
         print(Fore.RED + "Unable to use command: Nano is not installed!")
     
@@ -242,12 +288,18 @@ def reload():
     choice = input()
     
     if choice == 'Y' or choice == 'y': 
-        print(Fore.GREEN + "\nReloading CShell...")
-        command("cd " + currentDirectory + " && bash reload.sh")
-        sys.exit(0)
+        print("Reloading script...")
+        os.execv(sys.executable, ['python3'] + sys.argv)
     else:
         print(Fore.RED + "Aborted.")
 
+def script(scriptPath):
+    try:
+        with open(scriptPath, "r") as file:
+                for line in file:
+                    processCommand(line.strip())
+    except:
+        print(Fore.RED + "Error: File/directory not found! Try entering the path to the file again!")
 """
 Program
 """
@@ -268,10 +320,10 @@ else:
 if not isLinux:
 #             Color       Output
     print(Fore.CYAN + "This script is for " +
-            Fore.BLUE + "Linux " +
-            Fore.RED  + "only! " +
-            Fore.CYAN + "Either install Linux for edit the code " +
-            Fore.RED  + "(it may not work as expected or break if you edit the code).\n"  )
+          Fore.BLUE + "Linux " +
+          Fore.RED  + "only! " +
+          Fore.CYAN + "Either install Linux for edit the code " +
+          Fore.RED  + "(it may not work as expected or break if you edit the code).\n"  )
     sys.exit(1)
 
 #   Attempts to confirm Colorama's existence
@@ -285,7 +337,7 @@ except:
     coloramaInstalled = False
     
     print(Fore.RED + "Colorama doesn't appear to be installed!\n" +
-            Fore.RED + "Would you like to install it?\n(Y or N)\n")
+          Fore.RED + "Would you like to install it?\n(Y or N)\n")
     
     choice = input("\n")
     
@@ -301,9 +353,9 @@ if isLinux and sufficentVersion : help()
 while True and coloramaInstalled and isLinux and sufficentVersion and not locked:
 #                      Color        Output
     answer = input(Fore.BLUE   + 'CShell' +
-                    Fore.GREEN  + '$' +
-                    Fore.YELLOW + '~' +
-                    Fore.WHITE  + ': ')
+                   Fore.GREEN  + '$' +
+                   Fore.YELLOW + '~' +
+                   Fore.WHITE  + ': ')
     processCommand(answer)
 
 """
