5087e2ca358b5593baa3775f069dda3be0aeb598
Meme Guy <owen.debiasio@gmail.com>
2025-02-05 19:17:25 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
-echo "Would you like to install or uninstall CShell?"
-echo "(Y/N)"
-read action
+echo "Installing CShell..."
 
-if [[ "$action" == "Y" || "$action" == "y" ]]; then
-    echo "Installing CShell..."
+# Create cshell directory
+mkdir -p ~/cshell
 
-    # Create cshell directory
-    mkdir -p ~/cshell
-    
-    # Copy CShell script files
-    cp cshell.py ~/cshell/cshell.py
-    cp uninstall.sh ~/cshell/uninstall.sh
-    chmod +x ~/cshell/cshell.py
+# Copy CShell script files
+cp cshell.py ~/cshell/cshell.py
+cp uninstall.sh ~/cshell/uninstall.sh
+chmod +x ~/cshell/cshell.py
 
-    # Create a symbolic link in /usr/local/bin
-    sudo ln -sf ~/cshell/cshell.py /usr/local/bin/cshell
+# Create a symbolic link in /usr/local/bin
+sudo ln -sf ~/cshell/cshell.py /usr/local/bin/cshell
 
-    # Reload shell configuration
-    source ~/.bashrc
+# Reload shell configuration
+source ~/.bashrc
 
-    echo "CShell has been installed successfully. You can run it by typing 'cshell'."
-    
-else
-    echo "Invalid choice. Please type 'install' or 'uninstall'."
-fi
+echo "CShell has been installed successfully. You can run it by typing 'cshell'."
