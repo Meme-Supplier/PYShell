9aca62fb699e04f4534bcdd67f035a1b3ab49ab8
Meme Guy <owen.debiasio@gmail.com>
2025-02-06 08:49:03 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-echo "Installing CShell..."
+# Define color variables
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+RESET="\e[0m" # Reset color
+
+rm -r ~/cshell/
+
+echo -e "${BLUE}Installing CShell...${RESET}"
 
 # Create cshell directory
 mkdir -p ~/cshell
@@ -8,6 +19,8 @@ mkdir -p ~/cshell
 # Copy CShell script files
 cp cshell.py ~/cshell/cshell.py
 cp uninstall.sh ~/cshell/uninstall.sh
+cp upgrade.sh ~/cshell/upgrade.sh
+
 chmod +x ~/cshell/cshell.py
 
 # Create a symbolic link in /usr/local/bin
@@ -16,4 +29,5 @@ sudo ln -sf ~/cshell/cshell.py /usr/local/bin/cshell
 # Reload shell configuration
 source ~/.bashrc
 
-echo "CShell has been installed successfully. You can run it by typing 'cshell'."
+echo -e "${GREEN}CShell has been installed successfully. You can run it by typing 'cshell'.${RESET}"
+read x
