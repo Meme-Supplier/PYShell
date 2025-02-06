8dc64deba31560d1237738dda093da01baba6114
Meme Guy <owen.debiasio@gmail.com>
2025-02-06 14:46:08 -0500

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-# Define color variables
-RED="\e[31m"
+
 GREEN="\e[32m"
-YELLOW="\e[33m"
-BLUE="\e[34m"
-MAGENTA="\e[35m"
-CYAN="\e[36m"
 RESET="\e[0m" # Reset color
 
-echo -e "${BLUE}Upgrading CShell...${RESET}"
+echo -e "${GREEN}Upgrading CShell...${RESET}"
 
 sudo rm -r ~/CSHELL/
+
 git clone https://github.com/Meme-Supplier/CSHELL.git
 cd CSHELL
 chmod +x installer.sh
 ./installer.sh
+
+source ~/.bashrc
+
+rm -r ~/CSHELL/
+
+echo -e "${GREEN}CShell successfully updated!${RESET}"
 
 cshell
