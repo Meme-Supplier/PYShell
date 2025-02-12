4eb40ebf458d7a35d48c884e3bc6ab0f2ccea367
Meme Guy <owen.debiasio@gmail.com>
2025-02-12 17:42:44 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,27 +2,44 @@
 
 # Define color variables
 GREEN="\e[32m"
+BLUE="\e[34m"
+CYAN="\e[36m"
 RESET="\e[0m" # Reset color
 
-echo -e "${BLUE}Installing CShell...${RESET}"
+sudo echo ""
+echo -e "${GREEN}Installing CShell...${RESET}"
 
 if [ -d ~/cshell ]; then
+    echo -e "${CYAN}Removing \"$HOME/cshell/\"...${RESET}"
     rm -rf ~/cshell/
 fi
 
+echo -e "${CYAN}Creating \"$HOME/cshell/\"...${RESET}"
 mkdir -p ~/cshell
 
 # Copy CShell script files
+echo -e "${CYAN}Copying files...${RESET}"
+
+echo -e "${BLUE}Copying \"cshell.py\"...${RESET}"
 cp cshell.py ~/cshell/cshell.py
+
+echo -e "${BLUE}Copying \"uninstall.sh\"...${RESET}"
 cp uninstall.sh ~/cshell/uninstall.sh
+
+echo -e "${BLUE}Copying \"upgrade.sh\"...${RESET}"
 cp upgrade.sh ~/cshell/upgrade.sh
 
+echo -e "${CYAN}Successfully copied files.${RESET}"
+
+echo -e "${CYAN}Enabling execution for \"$HOME/cshell/cshell.py\"...${RESET}"
 chmod +x ~/cshell/cshell.py
 
 # Create a symbolic link in /usr/local/bin
+echo -e "${CYAN}Creating a symbolic link for \"$HOME/cshell/cshell.py\" in \"/usr/local/bin\"...${RESET}"
 sudo ln -sf ~/cshell/cshell.py /usr/local/bin/cshell
 
 # Reload shell configuration
+echo -e "${CYAN}Reloading shell configurations...${RESET}"
 source ~/.bashrc
 
 echo -e "${GREEN}CShell has been installed successfully. You can run it by typing 'cshell'.${RESET}"
