48262790ae30183bcc0cf2ff28e004be3a214c53
Meme Guy <owen.debiasio@gmail.com>
2025-02-13 19:56:28 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 # Define color variables
-RED="\e[31m"
 GREEN="\e[32m"
 BLUE="\e[34m"
 CYAN="\e[36m"
 RESET="\e[0m" # Reset color
 
-echo -e "${GREEN}Uninstalling CShell......${RESET}"
+sudo echo ""
+echo -e "${GREEN}Uninstalling CSHELL......${RESET}"
+sleep 1
 
 # Remove the symbolic link
 echo -e "${CYAN}Removing the symbolic link for \"$HOME/cshell/cshell.py\"...${RESET}"
 sudo rm -f /usr/local/bin/cshell
+sleep 0.5
 
 # Remove the CShell directory and files
 echo -e "${CYAN}Removing \"$HOME/cshell/\"...${RESET}"
 rm -rf ~/cshell
+sleep 0.5
 
 echo -e "${CYAN}Reloading shell configurations...${RESET}"
 source ~/.bashrc
+sleep 1
 
 # Confirm uninstallation
 echo -e "${CYAN}Confirming uninstallation...${RESET}"
+sleep 1
 
-if [[ ! -f /usr/local/bin/cshell && ! -d ~/cshell ]]; then
-    echo -e "${GREEN}CShell has been successfully uninstalled.${RESET}"
-else
-    echo -e "${RED}Error: CShell could not be completely removed.${RESET}"
-fi
+echo -e "${GREEN}CSHELL has been successfully uninstalled.${RESET}"
