341e7a48542feef305fe8fb18f39476e16ec5e7d
Meme Guy <owen.debiasio@gmail.com>
2025-02-28 15:43:07 -0500

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -5,20 +5,30 @@ RESET="\e[0m" # Reset color
 CYAN="\e[36m"
 
 sudo echo ""
-echo -e "${GREEN}Upgrading CSHELL...${RESET}"
+echo -e "${GREEN}Upgrading PYShell...${RESET}"
 
 cd ~/
 
-echo -e "${CYAN}Removing \"$HOME/CSHELL/\"...${RESET}"
-if [ -d ~/CSHELL ]; then
-    echo -e "${CYAN}Removing \"$HOME/CSHELL/\"...${RESET}"
-    rm -rf ~/CSHELL/
+echo -e "${CYAN}Removing \"$HOME/PYShell/\"...${RESET}"
+if [ -d ~/PYShell ]; then
+    echo -e "${CYAN}Removing \"$HOME/PYShell/\"...${RESET}"
+    rm -rf ~/PYShell/
 fi
 
-echo -e "${CYAN}Cloning into \"https://github.com/Meme-Supplier/CSHELL.git\"...${RESET}"
-git clone https://github.com/Meme-Supplier/CSHELL.git
+# Icon
+if [ -d /usr/share/applications/icon.png/ ]; then
+    echo -e "${CYAN}Removing \"/usr/share/applications/icon.png/\"...${RESET}"
+    sudo rm -f /usr/share/applications/icon.png/
+fi
 
-cd CSHELL
+# Desktop icons
+echo -e "${CYAN}Removing \"$HOME/Desktop/pyshell.desktop\"...${RESET}"
+sudo rm -f ~/Desktop/pyshell.desktop
+
+echo -e "${CYAN}Cloning into \"https://github.com/Meme-Supplier/PYShell.git\"...${RESET}"
+git clone https://github.com/Meme-Supplier/PYShell.git
+
+cd PYShell
 
 echo -e "${CYAN}Allowing execution for \"installer.sh\"...${RESET}"
 chmod +x installer.sh
@@ -29,6 +39,6 @@ echo -e "${CYAN}Running the installer...${RESET}"
 echo -e "${CYAN}Reloading shell configurations...${RESET}"
 source ~/.bashrc
 
-echo -e "${GREEN}CSHELL successfully updated!${RESET}"
+echo -e "${GREEN}PYShell successfully updated!${RESET}"
 
-cshell
+read
