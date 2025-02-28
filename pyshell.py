9dcb7ab6f89ecb0639f87d5af05309a3ac364fb6
Meme Guy <owen.debiasio@gmail.com>
2025-02-28 15:40:27 -0500

    Update and rename cshell.py to pyshell.py

--- pyshell.py
+++ pyshell.py
@@ -21,7 +21,7 @@ from pathlib import Path
 from readline import *
 from colorama import Fore, init
 
-# CSHELL modules
+# PYShell modules
 import cmdList
 import error
 import sysInfo
@@ -29,40 +29,32 @@ import logger
 
 init(autoreset = True)
 
-logger.log("CSHELL: Initialized modules.")
-
-# atexit
-def on_exit():
-    logger.log("\n====== Session exit: " +
-               logger.initTime() +
-               " ======")
-__import__("atexit").register(on_exit)
-logger.log("CSHELL: atexit registered")
+logger.log("PYShell: Initialized modules.")
 
 def ignoreCtrlC(signum, frame): ()
 __import__("signal").signal(__import__("signal").SIGINT, ignoreCtrlC)
-logger.log("CSHELL: Ctrl + C disabled")
+logger.log("PYShell: Ctrl + C disabled")
 
 pythonMajor = sys.version_info.major # Ex: 3.x.x
 pythonMinor = sys.version_info.minor # Ex: x.12.x
 pythonMicro = sys.version_info.micro # Ex: x.x.3
 pythonVersion = str(pythonMajor) + "." + str(pythonMinor) + "." + str(pythonMicro)
 pythonVersionShort = str(pythonMajor) + "." + str(pythonMinor)
-logger.log("CSHELL: Python version: " + pythonVersion)
+logger.log("PYShell: Python version: " + pythonVersion)
 
-cshellVer = "v1.8.5"
-logger.log("CSHELL: CSHELL version: " + cshellVer)
+pyshellVer = "v1.8.6"
+logger.log("PYShell: PYShell version: " + pyshellVer)
 
 locked = False
 passwordSet = False
 password = None
 
-logger.log("CSHELL: Variables initialized")
+logger.log("PYShell: Variables initialized")
 
 # Detecting if the system is linux
 if platform.system() == "Linux":
     sufficientPacMan = True
-    logger.log("CSHELL: System is Linux... Continuing")
+    logger.log("PYShell: System is Linux... Continuing")
 else:
     sufficientPacMan = False
     error.handle(11)
@@ -71,12 +63,12 @@ else:
 # Detecting a supported pac man
 if shutil.which("apt") or shutil.which("dnf") or shutil.which("pacman"):
     isLinux = True
-    logger.log("CSHELL: Sufficient package manager detected: Continuing...")
+    logger.log("PYShell: Sufficient package manager detected: Continuing...")
 else:
     isLinux = False
     error.handle(10)
 
-logger.log("CSHELL: CSHELL initialized")
+logger.log("PYShell: PYShell initialized")
 
 """
 Define
@@ -86,21 +78,21 @@ functions
 def processCommand(answer):
     # Goes through and executes commands
 
-    logger.log("CSHELL: Processing command: " + answer)
+    logger.log("PYShell: Processing command: " + answer)
     
     match answer:
-        case "clear"     : os.system('clear'); logger.log("CSHELL: Cleared the screen")
+        case "clear"     : os.system('clear'); logger.log("PYShell: Cleared the screen")
         case "help"      : help()
         case "python"    : command("python" + pythonVersionShort)
         case "cmds"      : cmdList.list()
-        case "exit"      : logger.log("CSHELL: Exiting CSHELL..."); sys.exit(0)
+        case "exit"      : logger.log("PYShell: Exiting PYShell..."); sys.exit(0)
         case "shutdown"  : command("shutdown now")
         case "reboot"    : command("reboot")
         case "ping"      : command("ping")
         case "credits"   : credits()
         case "ver"       : ver()
         case "update"    : update()  # updates your system
-        case "upgrade"   : upgrade()  # updates CSHELL
+        case "upgrade"   : upgrade()  # updates PYShell
         case "lock"      : lock()
         case "xray"      : edit()
         case "reload"    : reload()
@@ -110,7 +102,7 @@ def processCommand(answer):
         case "quit"      : quit()
         case "clean"     : clean()
         case "ip"        : command("hostname -I")
-        case "logs"      : command("nano ~/cshell/logs.txt")
+        case "logs"      : command("nano ~/pyshell/logs.txt")
         case "dellogs"   : dellogs()
         case "time"      : print(logger.initTime())
 
@@ -180,7 +172,7 @@ def processCommand(answer):
             else: 
                 print(Fore.RED + answer + ": invalid command.")
             
-            logger.log("CSHELL: Executed command: " + answer)
+            logger.log("PYShell: Executed command: " + answer)
 
 """
 Scripting
@@ -195,7 +187,7 @@ def expr():
 
 def dellogs():
 
-    command("rm ~/cshell/logs.txt -f")
+    command("rm ~/pyshell/logs.txt -f")
     logger.log("##### Initial log deletion #####")
     os.execv(sys.executable,
                 ["python3"] +
@@ -203,7 +195,7 @@ def dellogs():
 
 def web(page):
     webbrowser.open_new_tab(page)
-    logger.log("CSHELL: Opened webpage " + page)
+    logger.log("PYShell: Opened webpage " + page)
 
 def pm(cmd):
     if shutil.which("apt") or shutil.which("dnf") or shutil.which("pacman"):
@@ -218,7 +210,7 @@ def pm(cmd):
 def newdir(dir):
     Path(dir).mkdir(parents = True,
                     exist_ok = True)
-    logger.log("CSHELL: Directory created: " + dir)
+    logger.log("PYShell: Directory created: " + dir)
 
 def clean():
     if sufficientPacMan == True:
@@ -235,19 +227,19 @@ def clean():
 
         command("sudo rm -rf ~/.cache/*")
 
-        logger.log("CSHELL: Cleaned up cache")
+        logger.log("PYShell: Cleaned up cache")
 
 def delete(file):
     if os.path.exists(file):
         command("rm -rf " + file)
-        logger.log("CSHELL: Deleted file " + file)
+        logger.log("PYShell: Deleted file " + file)
     else:
         error.handle(2)
 
 def git():
     if gitInstalled():
         command(answer)
-        logger.log("CSHELL: Opened Git")
+        logger.log("PYShell: Opened Git")
     else:
         error.handle(3)
 
@@ -255,25 +247,25 @@ def uninstall():
     global uninstalled
     uninstalled = True
 
-    choice = input(Fore.RED + "Are you sure you want to uninstall CSHELL?\n" +
+    choice = input(Fore.RED + "Are you sure you want to uninstall PYShell?\n" +
                    Fore.WHITE)
 
     if choice == 'Y' or choice == 'y':
-        logger.log("CSHELL: Uninstalling: Sorry to see you go :(")
-        command("bash ~/cshell/uninstall.sh")
+        logger.log("PYShell: Uninstalling: Sorry to see you go :(")
+        command("bash ~/pyshell/uninstall.sh")
         sys.exit(0)
     else:
         print(Fore.GREEN + "\nAborted.")
-        logger.log("CSHELL: Aborted: Uninstall CSHELL")
+        logger.log("PYShell: Aborted: Uninstall PYShell")
 
 def wait(value):
     command("sleep " + str(value))
-    logger.log("CSHELL: Waited " + str(value) + "seconds")
+    logger.log("PYShell: Waited " + str(value) + "seconds")
 
 def command(command):
     subprocess.run([command],
                     shell = True)
-    logger.log("CSHELL: Ran shell command: " + command)
+    logger.log("PYShell: Ran shell command: " + command)
     
 def setPwd(pwd):
     global password
@@ -288,11 +280,11 @@ def setPwd(pwd):
         print(Fore.CYAN + "Password has been set to " +
               Fore.BLUE + password)
         
-        logger.log("CSHELL: Set password to " + password)
+        logger.log("PYShell: Set password to " + password)
 
 def lock():
     if passwordSet:
-        logger.log("CSHELL: Locked CSHELL")
+        logger.log("PYShell: Locked PYShell")
 
         os.system("clear")
         
@@ -304,7 +296,7 @@ def lock():
         while locked:
             
             if attemptsLeft != 0:
-                print(Fore.CYAN + "Enter password to unlock CSHELL.")
+                print(Fore.CYAN + "Enter password to unlock PYShell.")
                 pwdAttempt = input()
 
                 if pwdAttempt == password:
@@ -336,36 +328,36 @@ def update():
         error.handle(1)
 
 def upgrade():
-    print(Fore.CYAN + "Do you want to update CSHELL?\n" +
+    print(Fore.CYAN + "Do you want to update PYShell?\n" +
           Fore.BLUE + "(Y/N)\n")
 
-    print(Fore.RED + "Note: This will uninstall then reinstall CSHELL.")
+    print(Fore.RED + "Note: This will uninstall then reinstall PYShell.")
 
     choice = input()
 
     if choice == 'Y' or choice == 'y': 
-        logger.log("CSHELL: Upgrading CSHELL")
-        command("bash ~/cshell/upgrade.sh")
+        logger.log("PYShell: Upgrading PYShell")
+        command("bash ~/pyshell/upgrade.sh")
         sys.exit(0)
     else:
         print(Fore.RED + "Aborted.")
-        logger.log("CSHELL: Aborted: Upgrade CSHELL")
+        logger.log("PYShell: Aborted: Upgrade PYShell")
 
 def edit():
-    command("nano ~/cshell/cshell.py")
+    command("nano ~/pyshell/pyshell.py")
     print(Fore.BLUE + "\nChanges applied.")
-    logger.log("CSHELL: Made changes to CSHELL")
+    logger.log("PYShell: Made changes to PYShell")
     reload()
 
 def reload():
     
-    print(Fore.CYAN + "\nWould you like to reload CSHELL?")
+    print(Fore.CYAN + "\nWould you like to reload PYShell?")
     choice = input()
 
     if choice == 'Y' or choice == 'y': 
         print("Reloading script...")
 
-        logger.log("CSHELL: Reloaded CSHELL")
+        logger.log("PYShell: Reloaded PYShell")
         
         os.execv(sys.executable,
                 ["python3"] +
@@ -373,20 +365,20 @@ def reload():
         
     else:
         print(Fore.RED + "Aborted.")
-        logger.log("CSHELL: Aborted: Reload CSHELL")    
+        logger.log("PYShell: Aborted: Reload PYShell")    
 
 def script(scriptPath):
     
     if scriptPath.startswith("~/"):
         error.handle(8)
     else:
-        if answer.endswith(".cshell"):
+        if answer.endswith(".pyshell"):
             try:
                 with open(scriptPath, "r") as file:
                         for line in file:
                             processCommand(line.strip())
 
-                logger.log("CSHELL: Ran script: " + scriptPath)
+                logger.log("PYShell: Ran script: " + scriptPath)
             except:
                 error.handle(2)
         
@@ -403,17 +395,17 @@ def credits():
 def help():
     if sufficientPacMan and isLinux:
         print(Fore.CYAN   + "\nWelcome to " +
-              Fore.GREEN  + "CSHELL " +
-              Fore.YELLOW + cshellVer)
+              Fore.GREEN  + "PYShell " +
+              Fore.YELLOW + pyshellVer)
         print(Fore.CYAN   + "Type " +
               Fore.BLUE   + "\"cmds\"" +
               Fore.CYAN   + " for some commands!\n")
 
 def ver():
-#   CSHELL version
-    print(Fore.CYAN  + "\nCSHELL" +
+#   PYShell version
+    print(Fore.CYAN  + "\nPYShell" +
           Fore.BLUE  + " version: " +
-          Fore.GREEN + cshellVer)
+          Fore.GREEN + pyshellVer)
 
 #   Python version
     print(Fore.CYAN + "Python " +
@@ -441,7 +433,7 @@ def ver():
                                      '\n')
 
 def gitInstalled(): # Is git installed?
-    logger.log("CSHELL: Checking if git is installed...")
+    logger.log("PYShell: Checking if git is installed...")
 
     try:
         subprocess.run(["git", "--version"],
@@ -449,13 +441,13 @@ def gitInstalled(): # Is git installed?
                         stderr = subprocess.PIPE,
                         check  = True)
         
-        logger.log("CSHELL: Git is installer")
+        logger.log("PYShell: Git is installed")
 
         return True
     except (subprocess.CalledProcessError,
             FileNotFoundError):
         
-        logger.log("CSHELL: Git is not installed")
+        logger.log("PYShell: Git is not installed")
 
         return False
 
@@ -467,7 +459,7 @@ help()
 
 while True and isLinux and not locked and sufficientPacMan:
 
-    answer = input(Fore.BLUE  + "CSHELL" +
+    answer = input(Fore.BLUE  + "PYShell" +
                    Fore.GREEN + '$' +
                    Fore.CYAN  + '~' +
                    Fore.WHITE + ': ')
