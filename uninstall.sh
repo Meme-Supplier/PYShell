9eb74e1bd452cafcce42b0fb397ed396f03b403d
Meme Guy <owen.debiasio@gmail.com>
2025-03-02 17:19:46 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -7,11 +7,11 @@ CYAN="\e[36m"
 RESET="\e[0m" # Reset color
 
 sudo echo ""
-echo -e "${GREEN}Uninstalling PYShell......${RESET}"
+echo -e "${GREEN}Uninstalling PYShell......\n${RESET}"
 
 if [ -d ~/PYShell ]; then
-    echo -e "${CYAN}Removing \"$HOME/PYShell/\"...${RESET}"
-    rm -rf ~/PYShell/
+    echo -e "${CYAN}Removing \"$HOME/PYShell/\"...\n${RESET}"
+    sudo rm -rf ~/PYShell/
 fi
 
 # Remove the symbolic link
@@ -20,17 +20,17 @@ sudo rm -f /usr/local/bin/pyshell
 
 # Remove the PYShell directory and files
 echo -e "${CYAN}Removing \"$HOME/pyshell/\"...${RESET}"
-rm -rf ~/pyshell
+sudo rm -rf ~/pyshell
 
 # Icon
 echo -e "${CYAN}Removing \"/usr/share/applications/pyshell.png/\"...${RESET}"
-sudo rm -f /usr/share/applications/pyshell.png/
+sudo rm -f /usr/share/applications/pyshell.png
 
 # Desktop icons
 echo -e "${CYAN}Removing \"/usr/share/applications/pyshell.desktop\"...${RESET}"
 sudo rm -f /usr/share/applications/pyshell.desktop
 
-echo -e "${CYAN}Reloading shell configurations...${RESET}"
+echo -e "${CYAN}\nReloading shell configurations...${RESET}"
 source ~/.bashrc
 
 echo -e "${GREEN}\nPYShell has been successfully uninstalled.${RESET}"
